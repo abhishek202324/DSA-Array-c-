e067d5b84b1ee97a39b2177183d03ae31f1bb9de
ABHISHEK KUMAR <170253904+abhishek202324@users.noreply.github.com>
2025-05-27 17:35:48 +0530

    Add files via upload

---
+++ Q25ReplaceAllNegElementWithZero.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<climits> 
+using namespace std;
+
+void inputArray(int arr[], int size) {
+    cout << "Enter array elements: ";
+    for(int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+}
+void Original(int arr[], int size) {
+    cout<<"Original  array is : ";
+    for(int i =0; i<size; i++) {
+        cout<<arr[i]<<" ";
+}
+cout<<endl;
+    
+}
+
+void ReplaceNegtive(int arr[], int size) {
+    
+    for(int i=size-1;i>=1;i--){
+        if(arr[i]<0){
+            arr[i]=0;
+        }
+        
+    }
+    
+
+    cout<<"Array after  Replace All Negtive Element with zeros";
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+     cout<<endl;
+}
+    
+
+int main() {
+    int arr[8];
+    inputArray(arr, 8);
+    Original(arr,8);
+    ReplaceNegtive(arr, 8);
+  
+    return 0;
+}
