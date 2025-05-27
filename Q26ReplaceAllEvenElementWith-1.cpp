cc4485baedc12125bbe5b16a4398377917a5731a
ABHISHEK KUMAR <170253904+abhishek202324@users.noreply.github.com>
2025-05-28 00:22:52 +0530

    Add files via upload

---
+++ Q26ReplaceAllEvenElementWith-1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void inputArray(int arr[], int size) {
+    cout << "Enter array elements: ";
+    for(int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+}
+
+void Original(int arr[], int size) {
+    cout << "Original array is: ";
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void ReplaceEven(int arr[], int size) {
+    for(int i = 0; i < size; i++) {
+        if(arr[i] % 2 == 0) {
+            arr[i] = -1;
+        }
+    }
+
+    cout << "Array after replacing all even elements with -1: ";
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[8];
+    inputArray(arr, 8);
+    Original(arr, 8);
+    ReplaceEven(arr, 8);
+
+    return 0;
+}
