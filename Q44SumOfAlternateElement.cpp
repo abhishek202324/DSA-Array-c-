63fd3f381d0d52a089b9730c64a85873ea183258
ABHISHEK KUMAR <170253904+abhishek202324@users.noreply.github.com>
2025-05-29 17:10:51 +0530

    Add files via upload

---
+++ Q44SumOfAlternateElement.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void inputArray(int arr[], int size) {
+    cout << "Enter array elements: ";
+    for(int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+}
+
+void Original(int arr[], int size) {
+    cout << "Original array is: ";
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void SumOfAlternate(int arr[], int size) {
+    int sum = 0;
+    for(int i = 0; i < size; i += 2) {
+        sum += arr[i];
+    }
+    cout << "Sum of alternate elements (even indices): " << sum << endl;
+}
+
+int main() {
+    int arr[10];
+    inputArray(arr, 10);
+    Original(arr, 10);
+    SumOfAlternate(arr, 10);
+    return 0;
+}
