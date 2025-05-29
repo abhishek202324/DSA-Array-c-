2025b1f51f7082cd8ec2e541085591a071b268cf
ABHISHEK KUMAR <170253904+abhishek202324@users.noreply.github.com>
2025-05-29 17:09:55 +0530

    Add files via upload

---
+++ Q42FindMinimumInEvenPostion.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+void inputArray(int arr[], int size) {
+    cout << "Enter array elements: ";
+    for(int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+}
+
+void Original(int arr[], int size) {
+    cout << "Original array is: ";
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void MinEvenPos(int arr[], int size) {
+    int minVal = INT_MAX;
+    for(int i = 0; i < size; i += 2) {
+        if(arr[i] < minVal)
+            minVal = arr[i];
+    }
+    cout << "Minimum element at even positions: " << minVal << endl;
+}
+
+int main() {
+    int arr[10];
+    inputArray(arr, 10);
+    Original(arr, 10);
+    MinEvenPos(arr, 10);
+    return 0;
+}
