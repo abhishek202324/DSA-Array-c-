44b9283fd5432b6db1c35a77eab0c4298144cfb7
ABHISHEK KUMAR <170253904+abhishek202324@users.noreply.github.com>
2025-06-07 23:00:51 +0530

    Add files via upload

---
+++ Q64AfterRemovingAllOddNumber.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void inputArray(int arr[], int& size) {
+    cout << "Enter array elements: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+}
+
+void Original(int arr[], int size) {
+    cout << "Original array is: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void RemoveOdd(int arr[], int& size) {
+    int index = 0;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] % 2 == 0) {
+            arr[index++] = arr[i];
+        }
+    }
+    size = index;
+    cout << "Array after removing all odd numbers: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[10], size = 10;
+    inputArray(arr, size);
+    Original(arr, size);
+    RemoveOdd(arr, size);
+    return 0;
+}
